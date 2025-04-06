5140ade6f43653f40715d46ea3162b7676820ce5
eous <rosfloscampi@gmail.com>
2025-04-06 18:35:32 +0900

    Add files via upload

---
+++ 자기소개/script.js
@@ -0,0 +1,7 @@
+// 프로젝트 슬라이드 화살표 클릭 시 이벤트 (임시용)
+document.querySelectorAll(".project-page .arrow").forEach((btn) => {
+    btn.addEventListener("click", () => {
+      alert("슬라이드 이동 기능은 추후 구현 예정입니다.");
+    });
+  });
+  
